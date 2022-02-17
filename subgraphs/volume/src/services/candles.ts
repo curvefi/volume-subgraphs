2dbf96b2f637c14f1af05e3c3aa8945c4d381a0c
benber86 <benoitb@tutanota.de>
2022-02-18 10:05:59 +1100

    Update candle logic and keep tx count in each candle

--- subgraphs/volume/src/services/candles.ts
+++ subgraphs/volume/src/services/candles.ts
@@ -1,6 +1,6 @@
 import { BigDecimal, BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { Candle, Pool } from '../../generated/schema'
-import { BIG_DECIMAL_ZERO } from '../../../../packages/constants'
+import { BIG_DECIMAL_ZERO, BIG_INT_ONE } from '../../../../packages/constants'
 
 export function updateCandles(
   pool: Pool,
@@ -13,27 +13,22 @@ export function updateCandles(
   block: BigInt
 ): void {
   const periods: i32[] = [15 * 60, 60 * 60, 24 * 60 * 60, 7 * 24 * 60 * 60]
+  const pair = [token0.toHexString(), token1.toHexString()].sort().join('')
   for (let i = 0; i < periods.length; i++) {
     const time_id = timestamp.toI32() / periods[i]
-    const candle_id =
-      pool.id +
-      '-' +
-      token0.toHexString() +
-      token1.toHexString() +
-      '-' +
-      time_id.toString() +
-      '-' +
-      periods[i].toString()
+    const candle_id = pool.id + '-' + pair + '-' + time_id.toString() + '-' + periods[i].toString()
     let candle = Candle.load(candle_id)
-    if (candle === null) {
+    if (!candle) {
       candle = new Candle(candle_id)
-      candle.time = timestamp
+      candle.pool = pool.id
+      candle.timestamp = timestamp
       candle.period = periods[i]
       candle.token0 = token0
       candle.token1 = token1
       candle.open = price
       candle.low = price
       candle.high = price
+      candle.txs = BIG_INT_ONE
       candle.token0TotalAmount = BIG_DECIMAL_ZERO
       candle.token1TotalAmount = BIG_DECIMAL_ZERO
     } else {
@@ -43,6 +38,7 @@ export function updateCandles(
       if (price > candle.high) {
         candle.high = price
       }
+      candle.txs = candle.txs.plus(BIG_INT_ONE)
     }
 
     candle.close = price
