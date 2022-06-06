e46518e39ab2792bd6c4b6aae1e1ab1210158456
benber86 <benoitb@tutanota.de>
2022-06-06 21:25:16 +1000

    Initialize factory count to 0

--- subgraphs/volume/src/services/factory.ts
+++ subgraphs/volume/src/services/factory.ts
@@ -1,11 +1,12 @@
 import { Factory } from '../../generated/schema'
 import { Address } from '@graphprotocol/graph-ts'
-
+import { BIG_INT_ZERO } from 'const'
 
 export function getFactory(factoryAddress: Address): Factory {
   let factory = Factory.load(factoryAddress.toHexString())
   if (!factory) {
     factory = new Factory(factoryAddress.toHexString())
+    factory.poolCount = BIG_INT_ZERO
     factory.save()
   }
   return factory
