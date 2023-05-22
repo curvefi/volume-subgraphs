a2fe38bee0cd529618f7df0f17c5acda45c72ee2
benber86 <benoitb@tutanota.de>
2023-05-22 21:25:17 +1000

    Prevent eslint type removal

--- subgraphs/volume/src/services/factory.ts
+++ subgraphs/volume/src/services/factory.ts
@@ -2,7 +2,8 @@ import { Factory } from '../../generated/schema'
 import { Address } from '@graphprotocol/graph-ts'
 import { BIG_INT_ZERO } from 'const'
 
-export function getFactory(factoryAddress: Address, crvUsd = false): Factory {
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
+export function getFactory(factoryAddress: Address, crvUsd: boolean = false): Factory {
   let factory = Factory.load(factoryAddress.toHexString())
   if (!factory) {
     factory = new Factory(factoryAddress.toHexString())
