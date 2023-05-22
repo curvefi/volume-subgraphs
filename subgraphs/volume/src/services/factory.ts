63a42a8951eee4fdde822158bf7dad36f623dcdf
benber86 <benoitb@tutanota.de>
2023-05-22 21:22:44 +1000

    Add mixing type to signature

--- subgraphs/volume/src/services/factory.ts
+++ subgraphs/volume/src/services/factory.ts
@@ -2,12 +2,12 @@ import { Factory } from '../../generated/schema'
 import { Address } from '@graphprotocol/graph-ts'
 import { BIG_INT_ZERO } from 'const'
 
-export function getFactory(factoryAddress: Address, isCrvUsd = false): Factory {
+export function getFactory(factoryAddress: Address, crvUsd = false): Factory {
   let factory = Factory.load(factoryAddress.toHexString())
   if (!factory) {
     factory = new Factory(factoryAddress.toHexString())
     factory.poolCount = BIG_INT_ZERO
-    factory.crvUsd = isCrvUsd
+    factory.crvUsd = crvUsd
     factory.save()
   }
   return factory
