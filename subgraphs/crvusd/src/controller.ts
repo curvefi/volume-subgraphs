c3cae937f2c6b65464ef51bce289a7d5bee5c82b
benber86 <benoitb@tutanota.de>
2023-06-12 22:23:29 +1000

    Record collected fee event

--- subgraphs/crvusd/src/controller.ts
+++ subgraphs/crvusd/src/controller.ts
@@ -1,4 +1,13 @@
-import { Market, UserState, Borrow, Repayment, Removal, Liquidation, MonetaryPolicy } from '../generated/schema'
+import {
+  Market,
+  UserState,
+  Borrow,
+  Repayment,
+  Removal,
+  Liquidation,
+  MonetaryPolicy,
+  Snapshot, CollectedFee
+} from '../generated/schema'
 import {
   Borrow as BorrowEvent,
   RemoveCollateral as RemoveCollateralEvent,
@@ -10,7 +19,9 @@ import {
 import { getOrCreateUser } from './services/users'
 import { MonetaryPolicy as MonetaryPolicyTemplate } from '../generated/templates'
 import { Address, log } from '@graphprotocol/graph-ts'
-import { takeSnapshot } from './services/snapshot'
+import { takeSnapshot, toDecimal } from './services/snapshot'
+import { CollectFees } from '../generated/templates/Llamma/Controller'
+import { getIntervalFromTimestamp, HOUR } from './services/time'
 
 export function handleBorrow(event: BorrowEvent): void {
   const user = getOrCreateUser(event.params.user)
@@ -110,4 +121,34 @@ export function handleUserState(event: UserStateEvent): void {
   if (market) {
     takeSnapshot(Address.fromBytes(market.amm), event.block)
   }
+}
+
+export function handleCollectFees(event: CollectFees): void {
+  const market = Market.load(event.address)
+  if (!market) {
+    log.error("Unable to find market {} for collect fee events at {}", [event.address.toHexString(), event.transaction.hash.toHexString()])
+    return
+  }
+  // AMM fees are not logged so we retrieve them from latest snapshot
+  const hour = getIntervalFromTimestamp(event.block.timestamp, HOUR)
+  let id = market.amm.toHexString() + '-' + hour.toString()
+  let latestSnapshot = Snapshot.load(id)
+  if (!latestSnapshot) {
+    id = market.amm.toHexString() + '-' + hour.minus(HOUR).toString()
+    latestSnapshot = Snapshot.load(id)
+    if (!latestSnapshot) {
+      log.error("Could not find a snapshot within 2 hours for collect fees for {} at {} (time: {})", [event.address.toHexString(), event.transaction.hash.toHexString(), event.block.timestamp.toString()])
+      return
+    }
+  }
+  const feeCollected = new CollectedFee(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  feeCollected.market = event.address
+  feeCollected.borrowingFees = toDecimal(event.params.amount, "18")
+  feeCollected.ammCollateralFees = latestSnapshot.collateralAdminFees
+  feeCollected.ammCollateralFeesUsd = latestSnapshot.collateralAdminFees.times(latestSnapshot.oraclePrice)
+  feeCollected.ammBorrowingFees = latestSnapshot.crvUsdAdminFees
+  feeCollected.blockNumber = event.block.number
+  feeCollected.blockTimestamp = event.block.timestamp
+  feeCollected.transactionHash = event.transaction.hash
+  feeCollected.save()
 }
